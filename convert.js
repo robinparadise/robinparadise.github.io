991c133d9e1a5d71b39cfd615d24cd69140d672f
robinparadise <robinchogiles@gmail.com>
2024-11-13 11:33:12 +0100

    Desarrollo de interfaces v1.0

--- convert.js
+++ convert.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const path = require("path");
-const marked = require("marked");
-const hljs = require("highlight.js");
+// const marked = require("marked");
+const { Marked } = require("marked");
+const { markedHighlight } = require("marked-highlight");
+const hljs = require('highlight.js');
 
 // Array of directories containing Markdown files
 const inputDirs = ["./00 - Intro", "./01 - Interfaces"];
@@ -21,12 +23,42 @@ const htmlTemplate = (content) => `
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Markdown</title>
     <style>${githubCss}</style>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.7.0/highlight.min.js"></script>
+
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+      tailwind.config = {
+        theme: {
+          extend: {
+            colors: {
+              primary: '#0D3B66',
+              secondary: '#FAF0CA',
+            },
+            fontFamily: {
+              sans: ['Roboto', 'sans-serif'],
+            },
+          },
+        },
+      }
+    </script>
   </head>
   <body class="markdown-body">
     ${content}
+    <script src="https://unpkg.com/@highlightjs/cdn-assets@11.7.0/languages/css.min.js"></script>
   </body>
   </html>
 `;
+
+const marked = new Marked(
+  markedHighlight({
+  emptyLangClass: 'hljs',
+    langPrefix: 'hljs language-',
+    highlight(code, lang, info) {
+      const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+      return hljs.highlight(code, { language }).value;
+    }
+  })
+);
 
 
 // Function to convert Markdown to HTML
